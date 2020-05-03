4329e23d7e4c954c1c2183330e15cac09da9a3a2
Anowarul Kabir <akabir0101@gmail.com>
2020-05-03 00:19:27 -0400

    some importing codes added to test if packages are imported successfully

---
+++ tester.py
@@ -0,0 +1,11 @@
+# testing imports all packages
+import configs.general_config
+import data
+import datasets
+import metrics
+import models
+import utils
+import vizualizations.data_viz
+import vizualizations.output_viz
+
+print("Successfully imported all packages")
