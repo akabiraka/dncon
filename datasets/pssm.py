0e5afa02db112ee68db3b17209b3396066c8fc8d
Anowarul Kabir <akabir0101@gmail.com>
2020-05-26 15:09:19 -0400

    pssm refactored

--- datasets/pssm.py
+++ datasets/pssm.py
@@ -12,59 +12,60 @@ class PSSM(object):
         path = CONFIGS.PSIBLAST_DIR + pdb_code + chain_id + CONFIGS.DOT_PSSM
         df = pd.read_csv(path, delim_whitespace=True, header=None, skiprows=[1, 2]) # skipping 1st 2 rows
         df = df.head(-5) # removing last 5 rows
-        self.pssm = df.loc[:, 2:21]
-        self.observed_percentage = df.loc[:, 22:41]
-        self.information = df.loc[:, 42]
-        self.gapless_match = df.loc[:, 43]
-        self.full = df.loc[:, 2:]
-        self.full_minus_gapless_match = df.loc[:, 2:42]
+        return df
         
     def get_pssm(self, pdb_code, chain_id):
         """
         from psiblast output
         position-specific scoring matrix computed
         """
-        self.compute_things(pdb_code, chain_id)
-        return self.pssm
+        df = self.compute_things(pdb_code, chain_id)
+        pssm = df.loc[:, 2:21]
+        return pssm
 
     def get_observed_percentage(self, pdb_code, chain_id):
         """
         from psiblast output
         weighted observed percentages rounded down
         """
-        self.compute_things(pdb_code, chain_id)
-        return self.observed_percentage
+        df = self.compute_things(pdb_code, chain_id)
+        observed_percentage = df.loc[:, 22:41]
+        return observed_percentage
     
     def get_information(self, pdb_code, chain_id):
         """
         from psiblast output
         information per position
         """
-        self.compute_things(pdb_code, chain_id)
-        return self.information
+        df = self.compute_things(pdb_code, chain_id)
+        information = df.loc[:, 42]
+        return information
 
     def get_gapless_match(self, pdb_code, chain_id):
         """
         from psiblast output
         relative weight of gapless real matches to pseudocounts
         """
-        self.compute_things(pdb_code, chain_id)
-        return self.gapless_match
+        df = self.compute_things(pdb_code, chain_id)
+        gapless_match = df.loc[:, 43]
+        return gapless_match
     
     def get_full(self, pdb_code, chain_id):
         """
         returns full dataframe
         """
-        self.compute_things(pdb_code, chain_id)
-        return self.full
+        df = self.compute_things(pdb_code, chain_id)
+        full = df.loc[:, 2:]
+        return full
 
     def get_pssm_observed_percentage_information(self, pdb_code, chain_id):
         """
         returns pssm, weighted observed percentages and
         information per position
         """
-        self.compute_things(pdb_code, chain_id)
-        return self.full_minus_gapless_match 
+        df = self.compute_things(pdb_code, chain_id)
+        full_minus_gapless_match = df.loc[:, 2:42]
+        return full_minus_gapless_match 
 
 
 
