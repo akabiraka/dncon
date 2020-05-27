b3db72f1b827fc150817fb262c6070fed1d3e20b
Anowarul Kabir <akabir0101@gmail.com>
2020-05-27 19:10:04 -0400

    encode the separation of the residue pair in sequence added

--- datasets/global_features.py
+++ datasets/global_features.py
@@ -35,5 +35,28 @@ class GlobalFeatures(object):
         feature[:] = relative_pos
         return pd.DataFrame(feature)
 
-# gf = GlobalFeatures()
+    def __get_numeric_value(self, pos):
+        aa_separation_intervals = [1, 13, 19, 27, 39, 51, 63, 75, 87, 99, 111, 121]
+        for i, value in enumerate(aa_separation_intervals):
+            if i == 11:
+                return i
+            elif pos >= aa_separation_intervals[i] and pos < aa_separation_intervals[i+1]:
+                return i
+
+    def encode_residue_separation(self, aa_pos1, aa_pos2):
+        """
+        aa_pos1, aa_pos2: the positions of two amino-acid in protein sequence
+        """
+        encoded_pos1 = self.__get_numeric_value(aa_pos1)
+        encoded_pos2 = self.__get_numeric_value(aa_pos2)
+        encoded_pos1 = F.one_hot(torch.tensor(encoded_pos1), num_classes=12)
+        encoded_pos2 = F.one_hot(torch.tensor(encoded_pos2), num_classes=12)
+        return encoded_pos1 | encoded_pos2
+
+    def encode_residue_separation_df(self, aa_pos1, aa_pos2):
+        one_hot_tensor = self.encode_residue_separation(aa_pos1, aa_pos2)
+        return pd.DataFrame(one_hot_tensor.numpy())
+
+gf = GlobalFeatures()
 # gf.encode_window_position(5, 155)
+print(gf.encode_residue_separation_df(10, 28))
