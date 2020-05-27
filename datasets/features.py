f8a0ec084fe42ae7df65324bc7079d7c2b5fbb4a
Anowarul Kabir <akabir0101@gmail.com>
2020-05-27 14:23:38 -0400

    exposed alpha, beta residue percentage feature added

--- datasets/features.py
+++ datasets/features.py
@@ -20,18 +20,19 @@ class Features(object):
         pssm_df = self.pssm.get_pssm_observed_information(pdb_code, chain_id)
         ss3_df = self.scratch.get_secondary_structure_df(pdb_code, chain_id)
         acc2_df = self.scratch.get_solvency_accessibility_df(pdb_code, chain_id)
+        exposed_prcnt_df = self.scratch.get_exposed_percent_df(pdb_code, chain_id)
         encoded_prot_len = self.global_features.encode_protein_length(pssm_df.shape[0])
-        result = pd.concat([pssm_df, ss3_df, acc2_df, encoded_prot_len], axis=1, ignore_index=True)
+        result = pd.concat([pssm_df, ss3_df, acc2_df, exposed_prcnt_df, encoded_prot_len], axis=1, ignore_index=True)
         result = result.set_index(0)
 
         factors_df = pd.DataFrame()
         for aa in pssm_df.loc[:, 1]:
             factors_df = factors_df.append(self.atchley_factors.get(aa))
         
-        result = pd.concat([result, factors_df], axis=1, ignore_index=True)
-        # print(result)        
-        print(pssm_df.shape, ss3_df.shape, acc2_df.shape, factors_df.shape, \
-            encoded_prot_len.shape, result.shape)
+        result = pd.concat([result, factors_df], axis=1, ignore_index=True)       
+        print("PSSM: {}\nSS3: {}\nACC2: {}\nExposed %: {}\nProtein len: {}\nAtchley factors: {}\nCombined features: {}\n "\
+            .format(pssm_df.shape, ss3_df.shape, acc2_df.shape, exposed_prcnt_df.shape, \
+                encoded_prot_len.shape, factors_df.shape, result.shape))
         
         return result
 
--- datasets/scratch.py
+++ datasets/scratch.py
@@ -38,6 +38,33 @@ class SCRATCH(object):
         acc2_df = pd.DataFrame(acc2_tensor.numpy())
         return acc2_df
 
+    def get_exposed_percent(self, pdb_code, chain_id):
+        acc2_path = CONFIGS.SCRATCH_OUT + pdb_code + chain_id + CONFIGS.DOT_ACC
+        ss3_path = CONFIGS.SCRATCH_OUT + pdb_code + chain_id + CONFIGS.DOT_SS
+        acc2_data = self.__get_data(acc2_path)
+        ss3_data = self.__get_data(ss3_path)
+        exposed = ""
+        for i, ch in enumerate(acc2_data):
+            if ch == 'e':
+               exposed = exposed + ss3_data[i]
+        n_total = len(exposed)
+        perct_C = exposed.count('C') / n_total
+        perct_E = exposed.count('E') / n_total
+        perct_H = exposed.count('H') / n_total
+        exposed_percent = np.zeros((len(acc2_data), 3))
+        exposed_percent[:] = np.array([perct_C, perct_E, perct_H])
+        # print(broadcasted)
+        return exposed_percent
+        
+    def get_exposed_percent_df(self, pdb_code, chain_id):
+        exposed_percent = self.get_exposed_percent(pdb_code, chain_id)
+        return pd.DataFrame(exposed_percent)
+
+    def __get_data(self, path):
+        file_content = open(path, "r")
+        data = list(file_content)[1].rstrip("\n")
+        return data
+
 # scratch = SCRATCH()
 # print(scratch.get_secondary_structure("6y2d", "A"))
-# print(scratch.get_solvency_accessibility("6y2d", "A"))
+# print(scratch.get_exposed_percent("4eiu", "A"))
